bce0abde9b285fd5f477fe013537f8d8bd99c8a1
poulp <mathieu.nerv@gmail.com>
2013-11-21 17:37:46 +0100

    permier jet generation recette user

--- pweb/pweb/recette/utils.py
+++ pweb/pweb/recette/utils.py
@@ -1,15 +1,39 @@
+# -*- coding: utf-8 -*-
+
 import urllib2
 from rdflib import Graph, Namespace
 from rdflib.namespace import RDF, RDFS
+from random import choice
 
 
-NS1 = Namespace('http://www.random-food.com/')
+NS1 = Namespace('http://www.random-food.com/ontology#')
 LIRMM = Namespace('http://data.lirmm.fr/ontologies/food#')
 
-class Recipe:
+
+class Ingredient(object):
+    def __init__(self, name, unit, quantity):
+        self.name = name
+        self.unit = unit
+        self.quantity = quantity
+
+    def __unicode__(self):
+        return self.name
+
+
+class Utensil(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Recipe(object):
     def __init__(self):
         self.url = "http://localhost:5000/api/v1/recipe/gen"
         self.graph = Graph()
+
+        self.ingredient = []
+        self.utensil = []
+        self.nb_pers = 0
+
         self.request()
 
     def request(self):
@@ -19,8 +43,22 @@ class Recipe:
 
         self.graph.parse(data=response.read(), format="xml")
 
-        for s,p,o in self.graph.triples((None, RDF.type, LIRMM.Ingredient)):
-            self.graph.preferredLabel(s)
+        #ingredients
+        for s, p, o in self.graph.triples((None, RDF.type, LIRMM.Ingredient)):
+            self.ingredient.append(Ingredient(self.graph.value(s, RDFS.label), self.graph.value(s, LIRMM.unit), self.graph.value(s, LIRMM.quantity)))
+
+        #utensil
+        for s, p, o in self.graph.triples((None, RDF.type, NS1.Utensil)):
+            self.utensil.append(Utensil(self.graph.value(s, RDFS.label)))
+
+    def get_title(self):
+        ing1 = choice(self.ingredient)
+        ing2 = choice(self.ingredient)
+
+        a = ing1.__unicode__() + u" à la " + ing2.__unicode__()
+        print a
+        return a 
 
 if __name__ == "__main__":
     r = Recipe()
+    print r.utensil
--- pweb/pweb/recette/views.py
+++ pweb/pweb/recette/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render_to_response, RequestContext
+from utils import Recipe
+
 
 def home(request):
-    c = {}
-    return render_to_response('recette/home.html',c,RequestContext(request))
+    r = Recipe()
+    c = {
+        'recipe': r,
+    }
+    return render_to_response('recette/home.html', c, RequestContext(request))
