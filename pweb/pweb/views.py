45a634b4f9d8f610344191a6976ea350556dd2e3
poulp <mathieu.nerv@gmail.com>
2013-12-03 17:09:58 +0100

    ajout d'une page documentation pour l'api

--- pweb/pweb/views.py
+++ pweb/pweb/views.py
@@ -2,6 +2,27 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response, RequestContext
+import urllib2
+import json
+from utils import UrlDoc
+
+URL_DOC = "http://localhost:5000/api/v1/doc"
+
 
 def home(request):
-  return render_to_response('general/home.html',{},RequestContext(request))
+    return render_to_response('general/home.html', {}, RequestContext(request))
+
+
+def api_doc(request):
+    headers = {"Accept": "application/json"}
+    req = urllib2.Request(URL_DOC, headers=headers)
+    response = urllib2.urlopen(req)
+    doc_json = json.loads(response.read())
+    list_urls = []
+    for url in doc_json:
+        list_urls.append(UrlDoc(url, doc_json[url][0], doc_json[url][1]))
+
+    c = {
+        'urls': list_urls,
+    }
+    return render_to_response('general/documentation.html', c, RequestContext(request))
